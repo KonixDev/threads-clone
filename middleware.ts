c18f98eccec516911176b164e20f2a162dd08c04
Martin <martincoll000@gmail.com>
2024-01-09 20:21:33 -0300

    Fix

--- middleware.ts
+++ middleware.ts
@@ -2,10 +2,15 @@ import { authMiddleware } from "@clerk/nextjs";
 
 export default authMiddleware({
   // An array of public routes that don't require authentication.
-  publicRoutes: ["/api/webhook/clerk", "/api/uploadthing", '/sign-in', '/sign-up'],
+  publicRoutes: [
+    "/api/webhook/clerk",
+    "/api/uploadthing",
+    "/sign-in",
+    "/sign-up",
+  ],
 
   // An array of routes to be ignored by the authentication middleware.
-  ignoredRoutes: ["/api/webhook/clerk", '/sign-in', '/sign-up'],
+  ignoredRoutes: ["/api/webhook/clerk"],
 });
 
 export const config = {
